f2ec3f3a36134712228e276d2841c974dc9d04ee
piotr <piotrku91@github.com>
2020-12-05 13:47:23 +0100

    rasphberry pi qt installation script with updates

---
+++ rasphberry_qt_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get install g++
+echo $?
+sudo apt-get install qtcreator
+sudo apt install qtdeclarative5-dev
+sudo apt-get install qt5-qmake
+sudo apt-get install libqt5
+sudo apt-get install libqt5serialport5
+sudo apt-get install libqt5serialport5-dev
+sudo apt-get install qtmultimedia5-dev libqt5multimediawidgets5 libqt5multimedia5-plugins libqt5multimedia5
+sudo apt install qml-module-qtquick-controls2
+
+
+
